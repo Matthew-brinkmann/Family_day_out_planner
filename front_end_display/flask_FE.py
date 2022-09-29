20069d9b4fc46bf89ce4fbf16647c825ce58b59c
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-09-29 12:22:02 +1000

    update

--- front_end_display/flask_FE.py
+++ front_end_display/flask_FE.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Flask app to generate complete html page containing location/amenity
 dropdown menus and rental listings"""
+import os
 from flask import Flask, render_template
 app = Flask('web_dynamic')
 app.url_map.strict_slashes = False
@@ -10,3 +11,9 @@ app.url_map.strict_slashes = False
 def display_hbnb():
     """Generate page with popdown menu of states/cities"""
     return render_template('testFE_local.html')
+
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('FLASK_HOST') or '0.0.0.0',
+            port=os.getenv('FLASK_PORT') or '5000',
+            threaded=True)
