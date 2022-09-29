67446f969f995a9418c743f35e925f2c21b65513
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-09-29 12:10:30 +1000

    fix: syntax

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """module for flask app"""
 import os
-from flask import Flask, Blueprint, jsonify\
+from flask import Flask, Blueprint, jsonify
 from api.v1.views import app_views
 from flask_cors import CORS
 
