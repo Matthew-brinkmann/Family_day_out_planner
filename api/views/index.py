c16ae16e4c7925024836266c7faad719118df740
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-10-10 11:10:28 +1100

    refactor API

---
+++ api/views/index.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""index.py to connect to API"""
+from api.views import html_views
+from flask import Flask, Blueprint, render_template, jsonify
+
+
+@html_views.route('/status', strict_slashes=False)
+def apiStatus():
+    """API Status"""
+    return jsonify({"status": "OK"})
+
+
+@html_views.route('/', strict_slashes=False)
+def display_hbnb():
+    """Generate home page"""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    pass
