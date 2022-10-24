98af363dcfbf070180e8a5291d23147be1ee670b
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-10-24 11:31:54 +1100

    build in test endpoints

--- api/views/tests.py
+++ api/views/tests.py
@@ -17,3 +17,25 @@ def test_events():
     except TestRequestDataIncorrectFormat:
         abort(400, description="Data sent was not correct format")
     return(jsonify(json.dumps(test_return_dto)))
+
+@swag_from("documentation/login_success.yml", methods=['POST'])
+@app_views.route('test/login_success', methods=['POST'], strict_slashes=False)
+def test_events():
+    """ returns the test information """
+    request_data=request.get_json()
+    test_return_dto = {
+        "username": request_data["username"],
+        "password": request_data["password"],
+        "message": "login successful",
+        "token": "unique-token"
+    }
+    return(jsonify(json.dumps(test_return_dto)))
+
+@swag_from("documentation/login_fail.yml", methods=['POST'])
+@app_views.route('test/login_fail', methods=['POST'], strict_slashes=False)
+def test_events():
+    """ returns the test information """
+    test_return_dto = {
+        "message": "user/password not correct, please check details and try again"
+    }
+    return(jsonify(json.dumps(test_return_dto)))
